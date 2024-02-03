52b3040300b762f964a1c30a2cee81ed98b6dc62
autumnly1007 <autumnly1007@gmail.com>
2024-02-03 17:27:40 +0900

    [Feat] #8 로그인 화면 개발 및 서비스 연동

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { googleLogout } from '@react-oauth/google'; // 구글 로그아웃 테스트
+
+/**
+ * 구글 로그아웃 테스트 페이지
+ */
+const Logout: FC = () => {
+  return <div></div>;
+};
+
+export default Logout;
--- src/interfaces/components/button.interface.ts
+++ src/interfaces/components/button.interface.ts
@@ -1,3 +1,5 @@
+import { ButtonTheme } from '@/constants/ui-button.constant';
+
 /**
  * 공통 버튼 컴포넌트 Props
  */
@@ -5,5 +7,5 @@ export interface IButtonProps {
   text: string; // 텍스트
   onClick: () => void; // 클릭 이벤트 함수
   disabled?: boolean; // 활성화 여부
-  theme?: string; // 테마
+  theme?: ButtonTheme; // 테마
 }
